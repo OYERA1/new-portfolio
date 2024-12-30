0a3365b24385db7215d2b9ed6a3ed82292587f32
luan <oyera.dev@gmail.com>
2024-12-30 19:13:54 -0300

    idk

---
+++ src/components/header/index.tsx
@@ -0,0 +1,9 @@
+const Header = () => {
+	return (
+		<header className="container flex items-center justify-between rounded-xl bg-background/25 p-4 backdrop-blur-[2px]">
+			<nav className="flex items-center">Ø</nav>
+		</header>
+	);
+};
+
+export { Header };
