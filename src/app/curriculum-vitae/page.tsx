b04e51807d7637ab9426764622a0a5a35189e4d7
Luan <oyera.dev@gmail.com>
2025-01-31 01:39:17 -0300

    fix data

--- src/app/curriculum-vitae/page.tsx
+++ src/app/curriculum-vitae/page.tsx
@@ -1,14 +1,10 @@
 
-
-const CurriculumPage = () => {
+export default function CurriculumPage() {
     return (
         <div className="flex flex-col py-20">
             <h1 className="font-bold text-6xl">
                 Entre em contato
-
             </h1>
         </div>
     );
 };
-
-export { CurriculumPage };
